c6d4f27c146f8150c1baa880ca98840cf6416e8b
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-11-02 09:32:14 +0200

    Prediction of the user related movies

--- user_based_recom.py
+++ user_based_recom.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 ratings_data = pd.read_csv('ml-latest-small/ratings.csv')
 
 movies_data = pd.read_csv('ml-latest-small/movies.csv')
-ratings_with_name = pd.merge(ratings_data, movies_data).drop(['genres'], axis=1)
 
 def pearson_correlation(a, b, rated_intersection):
     average_a = float(sum(a['rating'])/len(a['rating']))
@@ -29,7 +28,7 @@ def pearson_correlation(a, b, rated_intersection):
     return sim_a_b
 
 
-def matching_users(input_user):
+def matching_users_dataframe(input_user):
     rated_all_users = ratings_data['userId'].unique()
     input_user_ratings = ratings_data[ratings_data['userId']==input_user]
 
@@ -45,15 +44,52 @@ def matching_users(input_user):
                 correlations_list.append(similarity_a_b)
                 similar_users.append(iterative_user_id)
 
-    dataframe_with_similar_data = pd.DataFrame(list(zip(similar_users, correlations_list)), columns = ['userId', 'correlations'])
-    most_similar_users_10 = dataframe_with_similar_data.nlargest(10, 'correlations')
+    dataframe_with_similar_data = pd.DataFrame(list(zip(similar_users, correlations_list)), columns = ['userId', 'correlation'])
 
-    return most_similar_users_10
+    return dataframe_with_similar_data
 
+
+def movie_predictions(user):
+    global top_similar_users
+
+    # Identifying the movie ids of the user that we are looking for
+    user_movie_ids = ratings_data[ratings_data['userId']==user].movieId
+
+    # Creating a dataframe with similar user's movie ratings only
+    top_similar_rated_movies = pd.DataFrame()
+    for similar_user in top_similar_users.userId:
+        top_similar_rated_movies = top_similar_rated_movies.append(ratings_data[ratings_data['userId'] == similar_user])
+
+    # making a list of movies that contains similar users movies, but not containing users rated movies
+    suggestable_movies = list()
+    for movie_id in top_similar_rated_movies.movieId.unique():
+        if movie_id not in user_movie_ids:
+            suggestable_movies.append(movie_id)
+
+    avg_of_user = float(np.mean(ratings_data[ratings_data.userId == user].rating))
+    denominator = float(sum(top_similar_users.correlation))
+    numerator = 0
+    recommendations = list()
+    top_movies = list()
+    for movie in suggestable_movies:
+        similar_movie = top_similar_rated_movies[top_similar_rated_movies['movieId']==movie]
+        for similar_movie_user in similar_movie['userId']:
+            avg_of_user_this_user = float(np.mean(ratings_data[ratings_data.userId == similar_movie_user].rating))
+            rbp = float(similar_movie[similar_movie['userId']==similar_movie_user].rating.values.tolist()[0])
+            correlation = float(top_similar_users[top_similar_users['userId'] == similar_user].correlation)
+            numerator += (correlation)*(rbp - avg_of_user_this_user)
+        prediction = avg_of_user + (numerator/denominator)
+        recommendations.append(prediction)
+        top_movies.append(movie)
+
+    movie_recommendations = pd.DataFrame(list(zip(top_movies, recommendations)), columns=['movie', 'score'])
+    return movie_recommendations
 
 
 
 def user_based_recommendation():
+    global top_similar_users
+
     print("====== First 10 ratings data items ======")
     print(ratings_data.head(10))
 
@@ -61,11 +97,21 @@ def user_based_recommendation():
     print(len(ratings_data))
 
     user_id_input = 10
-    most_similar_10 = matching_users(user_id_input)
-    print("====== 10 most similar users ======")
-    print(most_similar_10['userId'].values)
+    similar_user_results_count = 10
+    recommendable_movies_results_count = 20
 
+    matching_users_movies_data = matching_users_dataframe(user_id_input)
+    top_similar_users = matching_users_movies_data.nlargest(similar_user_results_count, 'correlation')
+    print("====== "+str(similar_user_results_count)+" most similar users ======")
+    print(top_similar_users['userId'].values)
 
+    print("====== "+str(recommendable_movies_results_count)+" recommending movies for the user "+str(user_id_input)+" ======")
+    movies_recommendation = movie_predictions(user_id_input)
+    top_prections = movies_recommendation.nlargest(recommendable_movies_results_count, 'score')
+    for predicted_movie in top_prections.movie:
+        print(movies_data[movies_data['movieId']==predicted_movie].title.values[0])
+
+    #print(top_prections['movie'].values)
 
 
 
