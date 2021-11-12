6e24f691d3e6b93a0796b3fb1d1882b02264b780
janakanawagamuwa <janakavishwajith@gmail.com>
2021-11-12 22:38:11 +0200

    Least misery aggregation integrated

--- group_recommendation.py
+++ group_recommendation.py
@@ -3,27 +3,46 @@ import numpy as np
 from user_based_recom import get_movie_recommendations
 from user_based_recom import movies_data
 
-def recommend_for_group(user_group):
 
+def recommend_for_group(user_group):
     recommendations_list = pd.DataFrame()
+
     for user_id in user_group:
+        # fetching movie recommendations for a given user
         user_movie_recommendations = get_movie_recommendations(user_id, 0)
-        # print(user_movie_recommendations)
+        # building data frame with users all recommendations for movies
         recommendations_list = recommendations_list.append(user_movie_recommendations, ignore_index=True)
 
     # recommendations_list = recommendations_list[recommendations_list.groupby(['movie'])['movie'].transform('size') > 2]
-    # print(recommendations_list)
     # TODO: Assumption to mention
     # Keep only duplicated ones (rated by more than 1 users in the similar users list)
-    # recommendations_list = recommendations_list[recommendations_list.duplicated(subset=['movie'], keep=False)]
     recommendations_list = recommendations_list[recommendations_list.groupby('movie')['movie'].transform('size') == len(user_group)]
 
-    # Calculating average
+    print('\n======= Movie list according to Average Aggregation =======')
+    avg_aggregation_df = average_aggregation(recommendations_list)
+    for predicted_movie in avg_aggregation_df.movie:
+        print(movies_data[movies_data['movieId']==predicted_movie].title.values[0])
+
+    print('\n\n======= Movie list according to Least Misery Aggregation =======')
+    least_misery_aggregation_df = least_misery_aggregation(recommendations_list)
+    for predicted_movie in least_misery_aggregation_df.movie:
+        print(movies_data[movies_data['movieId']==predicted_movie].title.values[0])
+
+
+# Function that does average aggregation of the given list
+def average_aggregation(recommendations_list):
+    # Calculating average rating of the given movie
     recommendations_list = recommendations_list.groupby('movie')['score'].mean().reset_index()
     recommendations_list = recommendations_list.nlargest(20, 'score')
+    return recommendations_list
 
-    for predicted_movie in recommendations_list.movie:
-        print(movies_data[movies_data['movieId']==predicted_movie].title.values[0])
+
+# Function that does least misery aggregation of the given list
+def least_misery_aggregation(recommendations_list):
+    # Calculating minimum rating of the given movie
+    recommendations_list = recommendations_list.groupby('movie')['score'].min().reset_index()
+    recommendations_list = recommendations_list.nlargest(20, 'score')
+    return recommendations_list
 
 
 recommend_for_group([9,31,33])
