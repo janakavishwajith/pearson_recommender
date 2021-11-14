1dc48f0d96a999c8ad8645e51309029ffb82b341
janakanawagamuwa <janakavishwajith@gmail.com>
2021-11-15 00:55:03 +0200

    Integrating kendall tau disagreement calculation

--- group_recommendation.py
+++ group_recommendation.py
@@ -2,17 +2,24 @@ import pandas as pd
 import numpy as np
 from user_based_recom import get_movie_recommendations
 from user_based_recom import movies_data
+from itertools import combinations
+import scipy.stats as stats
 
 
-def recommend_for_group(user_group):
+def user_wise_recommendations(user_group):
     recommendations_list = pd.DataFrame()
 
     for user_id in user_group:
         # fetching movie recommendations for a given user
         user_movie_recommendations = get_movie_recommendations(user_id, 0)
+        user_movie_recommendations = user_movie_recommendations.assign(user=user_id)
         # building data frame with users all recommendations for movies
         recommendations_list = recommendations_list.append(user_movie_recommendations, ignore_index=True)
+    return recommendations_list
 
+
+def recommend_for_group(user_group):
+    recommendations_list = user_wise_recommendations(user_group)
     # recommendations_list = recommendations_list[recommendations_list.groupby(['movie'])['movie'].transform('size') > 2]
     # TODO: Assumption to mention
     # Keep only duplicated ones (rated by more than 1 users in the similar users list)
@@ -45,4 +52,31 @@ def least_misery_aggregation(recommendations_list):
     return recommendations_list
 
 
-recommend_for_group([9,31,33])
+# recommend_for_group([9,31,33])
+
+# Part B
+# Need a function to calculate the distance of the recommendations.
+def recommendation_with_distance(input_users):
+    user_wise_recoms = user_wise_recommendations(input_users)
+
+    kendall_tau_output = pd.DataFrame(columns=['user1', 'user2', 'disagreement'])
+    for combo in combinations(input_users, 2):
+        user1_data = user_wise_recoms[user_wise_recoms['user']==combo[0]]
+        user2_data = user_wise_recoms[user_wise_recoms['user']==combo[1]]
+        user1_data = user1_data.sort_values(by=['score']).movie
+        user2_data = user2_data.sort_values(by=['score']).movie
+
+        # Shortening dataframes to match common length
+        if user1_data.size > user2_data.size:
+            user1_data = user1_data[:user2_data.size]
+        elif user2_data.size > user1_data.size:
+            user2_data = user2_data[:user1_data.size]
+
+        kendall_distance = stats.kendalltau(user1_data, user2_data).correlation
+        kendall_tau_output = kendall_tau_output.append({'user1': combo[0], 'user2': combo[1], 'disagreement':kendall_distance}, ignore_index=True)
+    print(kendall_tau_output)
+
+
+recommendation_with_distance([9,31,33])
+# test([9,31,33])
+
