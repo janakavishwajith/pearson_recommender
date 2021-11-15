eab4b5525b9c6b2792d98ec647ce979e4b630ea5
janakanawagamuwa <janakavishwajith@gmail.com>
2021-11-16 00:22:51 +0200

    Kendal tau and distance considered group recommendation

--- group_recommendation.py
+++ group_recommendation.py
@@ -15,15 +15,15 @@ def user_wise_recommendations(user_group):
         user_movie_recommendations = user_movie_recommendations.assign(user=user_id)
         # building data frame with users all recommendations for movies
         recommendations_list = recommendations_list.append(user_movie_recommendations, ignore_index=True)
+    # TODO: Assumption to mention
+    # Keep only duplicated ones (rated by more than 1 users in the similar users list)
+    recommendations_list = recommendations_list[recommendations_list.groupby('movie')['movie'].transform('size') == len(user_group)]
+
     return recommendations_list
 
 
 def recommend_for_group(user_group):
     recommendations_list = user_wise_recommendations(user_group)
-    # recommendations_list = recommendations_list[recommendations_list.groupby(['movie'])['movie'].transform('size') > 2]
-    # TODO: Assumption to mention
-    # Keep only duplicated ones (rated by more than 1 users in the similar users list)
-    recommendations_list = recommendations_list[recommendations_list.groupby('movie')['movie'].transform('size') == len(user_group)]
 
     print('\n======= Movie list according to Average Aggregation =======')
     avg_aggregation_df = average_aggregation(recommendations_list)
@@ -52,17 +52,32 @@ def least_misery_aggregation(recommendations_list):
     return recommendations_list
 
 
-# recommend_for_group([9,31,33])
+recommend_for_group([9,31,33])
+
 
 # Part B
-# Need a function to calculate the distance of the recommendations.
 def recommendation_with_distance(input_users):
     user_wise_recoms = user_wise_recommendations(input_users)
+    kendall_tau_output = get_kendall_tau_disagreements(input_users, user_wise_recoms)
+    print('\n======= Kendal tau calculation output =======')
+    print(kendall_tau_output)
+    for user in input_users:
+        fair_points = kendall_tau_output[kendall_tau_output['user1'] == user].disagreement.sum()
+        fair_points += kendall_tau_output[kendall_tau_output['user2'] == user].disagreement.sum()
+        cond = user_wise_recoms['user']==user
+        user_wise_recoms.loc[cond,'score'] -= fair_points
 
+    print('\n======= Average aggregation after considering distances =======')
+    avg_aggregation_df = average_aggregation(user_wise_recoms)
+    for predicted_movie in avg_aggregation_df.movie:
+        print(movies_data[movies_data['movieId'] == predicted_movie].title.values[0])
+
+
+def get_kendall_tau_disagreements(input_users, user_wise_recoms):
     kendall_tau_output = pd.DataFrame(columns=['user1', 'user2', 'disagreement'])
     for combo in combinations(input_users, 2):
-        user1_data = user_wise_recoms[user_wise_recoms['user']==combo[0]]
-        user2_data = user_wise_recoms[user_wise_recoms['user']==combo[1]]
+        user1_data = user_wise_recoms[user_wise_recoms['user'] == combo[0]]
+        user2_data = user_wise_recoms[user_wise_recoms['user'] == combo[1]]
         user1_data = user1_data.sort_values(by=['score']).movie
         user2_data = user2_data.sort_values(by=['score']).movie
 
@@ -73,9 +88,8 @@ def recommendation_with_distance(input_users):
             user2_data = user2_data[:user1_data.size]
 
         kendall_distance = stats.kendalltau(user1_data, user2_data).correlation
-        kendall_tau_output = kendall_tau_output.append({'user1': combo[0], 'user2': combo[1], 'disagreement':kendall_distance}, ignore_index=True)
-    print(kendall_tau_output)
-
+        kendall_tau_output = kendall_tau_output.append({'user1': combo[0], 'user2': combo[1], 'disagreement': kendall_distance}, ignore_index=True)
+    return kendall_tau_output
 
 recommendation_with_distance([9,31,33])
 # test([9,31,33])
