f8fd19631a2768c3cd3eb846e9d495ca6eb2d4c5
janakanawagamuwa <janakavishwajith@gmail.com>
2021-12-08 12:28:36 +0200

    Why not update for the movie genre

--- why_not_question.py
+++ why_not_question.py
@@ -55,7 +55,7 @@ def user_why_not_movie(user_id, movie):
         if user_x_rating_on_movie[user_x_rating_on_movie['rating'] < 3].size < (matching_users.size/2):
             print('WHY-NOT? More than 50% of the similar users have given low ratings for the movie.')
 
-
+print('\n================ Group recommendation for the user group [2,10,22] why not present movieId 21 as a suggestion ================')
 why_not_movie([2,10,22], 21)
 
 #########################
@@ -123,7 +123,8 @@ def user_why_not_genre(user_id, genre):
         else:
             print('Reasonable ratings for the similar users for the genre ', genre)
 
-# why_not_genre([2,10,22], 'Documentary')
+print('\n================ Group recommendation for the user group [2,10,22] why not present Documentary genre in suggestions ================')
+why_not_genre([2,10,22], 'Documentary')
 
 
 ##############################
@@ -131,31 +132,14 @@ def user_why_not_genre(user_id, genre):
 ##############################
 def why_not_position_x(user_group, movie, position):
     recommendations_list = user_wise_recommendations(user_group, False)
+    aggregated_recommendations_list = recommendations_list.groupby('movie')['score'].mean().reset_index()
 
-    existing_in_suggestion = recommendations_list[recommendations_list['movie'] == movie]
-    print(existing_in_suggestion.user.tolist())
-    suggestion_not_exists = pd.DataFrame(columns=['user'])
-    for group_user in user_group:
-        if group_user not in existing_in_suggestion.user.tolist():
-            suggestion_not_exists = suggestion_not_exists.append({'user': group_user}, ignore_index=True)
+    aggregated_recommendations_list = aggregated_recommendations_list.sort_values(by='score', ascending=False, ignore_index=True)
+    given_movie_index = aggregated_recommendations_list[aggregated_recommendations_list['movie'] == movie].index[0]
 
-    # This will check if any of the group users has not been suggested the particular movie
-    # TODO: Uncomment
-    if not suggestion_not_exists.empty:
-        print('========= Movie ', movies_data[movies_data['movieId'] == movie].title.values[0],
-              ' has not been suggested to user(s) ', suggestion_not_exists.user.tolist(), ' of the group.')
-        for movie_not_suggested_user in suggestion_not_exists.user.tolist():
-            user_why_not_movie(movie_not_suggested_user, movie)
+    print(aggregated_recommendations_list)
+    print('Given movie index ', given_movie_index)
+    print('Movie in the given index ', aggregated_recommendations_list.loc[position].movie)
 
-        # This should go through the user level why-not question
-
-    # TODO: Uncomment
-    if suggestion_not_exists.empty:
-        aggregated_results = average_aggregation(recommendations_list)
-        lowest_score = aggregated_results.score.min()
-
-        for existing_rating_user in existing_in_suggestion.user:
-            if existing_in_suggestion[existing_in_suggestion['user'] == existing_rating_user].score.values[0] < lowest_score:
-                print('\nWHY-NOT? User ', existing_rating_user, ' suggestion score is lower than lowest score...')
-
-
+print('\n================ Group recommendation for the user group [2,10,22] why not present movieId 21 at the position 3 ================')
+why_not_position_x([2,10,22], 21, 3)
